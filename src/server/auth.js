674144871fec689144c697689c833ee53cad5172
Shawn Watson <shawnw86@gmail.com>
2016-06-02 13:25:30 -0700

    Remove remainder of facebook logic, add jwt-simple module

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,16 +1,11 @@
 var secret = require('../../secret.js')
 
 module.exports = {
-	'facebookAuth': {
-		'clientID'			: '702348803238668',
-		'clientSecret'	    : secret.fbId,
-		'callbackURL'		: 'http://localhost:3000/auth/facebook/callback'
-	},
-    'githubAuth': {
-        'clientID'          : '9078c5924546759bdff3',
-        'clientSecret'      : secret.githubId,
-        'callbackURL'       : 'http://localhost:3000/auth/github/callback'
-    }
+	'githubAuth': {
+		'clientID'          : '9078c5924546759bdff3',
+		'clientSecret'      : secret.githubId,
+		'callbackURL'       : 'http://localhost:3000/auth/github/callback'
+	}
 }
 
 
